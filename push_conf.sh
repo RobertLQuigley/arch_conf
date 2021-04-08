394015f1ad2ba5f3639e7278bf31114dd203e377
Robert Quigley <quigley.robertl@gmail.com>
2021-04-08 13:56:19 -0400

    Teach to save/deploy

--- push_conf.sh
+++ push_conf.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
+push=0
+save=0
+
+while getopts :ps flag
+do
+	case "${flag}" in
+		p)	push=1
+			;;
+		s)  save=1
+			;;
+		\?) echo -e "Usage: push_conf.sh [-p] [-s]";
+			echo -e "\t-p\tPush storage config to live if out of sync";
+			echo -e "\t-s\tSave live config to storage if out of sync";
+			exit
+			;;
+	esac
+
+done
+
+echo -e $push
+echo -e $save
+
 declare -A confmap
 
 confmap[bashrc]="$HOME/.bashrc"
 confmap[i3_config]="$HOME/.config/i3/config"
-confmap[vimrc]="$Home/.vimrc"
+confmap[vimrc]="$HOME/.vimrc"
 
 function compare_md5() {
 	md5_lhs=$(md5sum ./$1 | cut -d " " -f1)
@@ -22,16 +44,33 @@ function compare_md5() {
 	fi
 }
 
-for src in "${!confmap[@]}"; 
+for storage in "${!confmap[@]}"; 
 do
-	echo -e $src;
-	echo -e ${confmap[$src]};
-	compare_md5 "$src" "${confmap[$src]}";
+	live=${confmap[$storage]};
+	compare_md5 "$storage" "$live";
 	if [[ $? -eq 0 ]];
 	then
 		echo -e "Nothing to do";
 	else
-		echo -e "Need to push";
+		case 1 in
+			$push )
+				echo "Pushing live config to storage..."
+				cp "$storage" "$live"
+				;;
+			$save )
+				echo "Saving live config..."
+				cp "$live" "$storage";
+				;;
+			* )
+				echo "Info for $storage and $live:";
+				if [[ $storage -nt $live} ]];
+				then
+					echo -e "$storage is newer."
+				else 		
+					echo -e "$live is newer."
+				fi
+				;;
+		esac	
 	fi
 done
 
